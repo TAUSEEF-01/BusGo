cd78d775c3a33271183f1f94048a31958839eeb3
TAUSEEF-01 <thesupremeemperor01@gmail.com>
2026-07-06 23:13:36 +0600

    Deploy: Update REPO_URL in setup_server.sh to match correct repository

--- setup_server.sh
+++ setup_server.sh
@@ -8,7 +8,7 @@ set -e
 
 SERVER_IP="135.171.216.245"
 DOMAIN="busgo.farefin.com"
-REPO_URL="https://github.com/TAUSEEF-01/BusGo.git"
+REPO_URL="https://github.com/TAUSEEF-01/Jaabo.git"
 PROJECT_DIR="$HOME/Jaabo"
 
 echo "=================================================="
